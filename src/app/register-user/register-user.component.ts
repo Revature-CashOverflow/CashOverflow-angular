4bccf431a513cdd346d759e31e6f4dd12f178c72
cameronaumann <cameron.aumann1@gmail.com>
2022-02-09 14:47:27 -0800

    created a register component

---
+++ src/app/register-user/register-user.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FormBuilder } from '@angular/forms';
+
+@Component({
+  selector: 'app-register-user',
+  templateUrl: './register-user.component.html',
+  styleUrls: ['./register-user.component.css']
+})
+export class RegisterUserComponent implements OnInit {
+
+  newUser() {
+    
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
