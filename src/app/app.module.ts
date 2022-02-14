494d65318b19eca63a5e0d3ceef7ca523d4b1f76
Tyler Rondeau <rondeautyler96@gmail.com>
2022-02-14 18:05:31 -0500

    cookies!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './components/login/login.component';
 import { HttpClientModule } from '@angular/common/http';
-import { LoginService } from './login.service'; 
+import { LoginService } from './service/login/login.service'; 
 import { ReactiveFormsModule } from '@angular/forms';
+import { CookieService } from 'ngx-cookie-service';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,10 @@ import { ReactiveFormsModule } from '@angular/forms';
     HttpClientModule,
     ReactiveFormsModule
   ],
-  providers: [LoginService],
+  providers: [
+    LoginService, 
+    CookieService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/model/jwt.ts
@@ -0,0 +1,3 @@
+export interface jwtDto{
+    jwt:string
+}
