764dbc7766c08fca48a703cda822a5ce14cd9f51
hgharvil <hg.harvil@gmail.com>
2022-02-15 18:00:45 -0500

    added background for pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { CreateBankAccountFormComponent } from './createBankAccountForm/create-b
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { FeedComponent } from './feed/feed.component';
 import { LoginComponent } from './login/login.component';
+import { RegisterUserComponent } from './register-user/register-user.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component:LoginComponent },
   { path: 'feed', component: FeedComponent },
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'createBankAccountForm', component: CreateBankAccountFormComponent }
+  { path: 'createBankAccountForm', component: CreateBankAccountFormComponent },
+  { path: 'register', component: RegisterUserComponent }
 ];
 
 @NgModule({
