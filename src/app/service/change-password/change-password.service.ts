12e9f99b251e5adb12a55243d058b453015c1761
Micheal Bailey <mbaileyfuturist@gmail.com>
2022-04-06 19:45:09 -0400

    Created a ChangePassword component that displays a form with two inputs. One for the new password and the other to retype the new password. Created a settings page that will be used as a route to display this component. Added a settings tab to the general navigation bar. Included validation logic, all new passwords must contain two special characters, at least two numbers and at least two capital letters.

--- src/app/service/change-password/change-password.service.ts
+++ src/app/service/change-password/change-password.service.ts
@@ -2,24 +2,25 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 
+
 @Injectable({
   providedIn: 'root',
 })
-export class RegisterService {
+export class ChangePasswordService {
   constructor(private http: HttpClient) {}
 
   /**
    * Added headers to cope with CORS errors
    * @author Cameron, Amir, Chandra
    */
-  sendRegisterData(registerForm) {
+  sendPasswordData(changePasswordForm) {
 
     const headers= new HttpHeaders()
     .set('content-type', 'application/json')
-    .set('Access-Control-Allow-Origin', `${environment.apiURL}/register`);
+    .set('Access-Control-Allow-Origin', `${environment.apiURL}/changePassword`);
      let options = { headers: headers };
 
-    return this.http.post(`${environment.apiURL}/register`, registerForm, options);
+    return this.http.post(`${environment.apiURL}/changePassword`, changePasswordForm, options);
 
   }
 }
